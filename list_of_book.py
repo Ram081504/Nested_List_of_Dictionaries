8a9050ce3a65cedfaa3b2e9f7c717e67d38a9209
Ram081504 <ramlouisecapilitan015@gmail.com>
2024-10-13 10:57:07 +0800

    Nested List of Dictionaries

---
+++ list_of_book.py
@@ -0,0 +1,78 @@
+products = [
+{
+"id": 1,
+"name": "IPHONE 13",
+"category": "Electronics",
+"price" : 2500,
+"colors" : ["gray", "black"],
+"size": ["small", "medium", "large"],
+"stock": [{
+"gray-small" : 25,
+"gray-medium" : 30,
+"gray-large" : 34
+},
+{
+"black-small": 23,
+"black-medium": 23,
+"black-large": 25
+}],
+},
+
+{
+"id": 2,
+"name": "IPHONE 15",
+"category":"Electronics",
+"price" : 2500,
+"colors" : ["gray", "black"],
+"size": ["small", "medium", "large"],
+"stock": [{
+"gray-small" : 25,
+"gray-medium" : 30,
+"gray-large" : 34
+},
+{
+"black-small": 23,
+"black-medium": 23,
+"black-large": 25
+}],
+},
+
+{
+"id": 3,
+"name": "IPHONE 16",
+"category":"Electronics",
+"price" : 2500,
+"colors" : ["gray", "black"],
+"size": ["small", "medium", "large"],
+"stock": [{
+"gray-small" : 25,
+"gray-medium" : 30,
+"gray-large" : 34
+},
+{
+"black-small": 23,
+"black-medium": 23,
+"black-large": 25
+}],
+}
+]
+print(products)
+
+products[2]["name"] = "IPHONE 16 PROMAX"
+
+
+
+#ACCESS STOCK OF products[2]
+print("Stock of products index 2", products[2]["stock"]) 
+
+
+ 
+
+#ACCESS INDEX 3 STOCK OF products[2]
+print("Stock of products index 2", products[2]["stock"][1]) 
+
+#TODO CHANGE black small quantity - products[2]
+products[2]["stock"][1]["black-small"] = 22 
+print("Updated Stock of products index 2", products[2]["stock"][1])
+rodneybuy = products[2]
+#print(rodneybuy)
